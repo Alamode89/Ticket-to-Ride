e80d4b384da24623301a48b96850cf3eab16d7c2
Liberty <libisin@gmail.com>
2021-02-23 20:12:51 -0800

    Stub game, board, city, map

---
+++ city.hpp
@@ -0,0 +1,19 @@
+#ifndef __CITY_HPP__
+#define __CITY_HPP__
+
+#include <string>
+
+using namespace std;
+
+class City {
+  //image
+  //int nodeNumber?
+  //City Name
+  public: 
+   string destinationName;
+
+
+
+};
+
+#endif //__CITY_HPP__
---
+++ map.hpp
@@ -0,0 +1,16 @@
+#ifndef __MAP_HPP__
+#define __MAP_HPP__
+
+#include <vector>
+#include "city.hpp"
+
+using namespace std;
+
+class MAP {
+  //image
+  string imagePath="";
+  vector<City> destinations;
+
+};
+
+#endif //__MAP_HPP__
