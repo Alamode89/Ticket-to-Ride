622b54a6b3697111f72955d8fe542a255652128e
Alamode89 <andreamante89@gmail.com>
2021-02-23 21:02:16 -0800

    Added player and deck class stubs. Imported GoogleTest

---
+++ deck.hpp
@@ -0,0 +1,14 @@
+#ifndef __DECK_HPP__
+#define __DECK_HPP__
+
+#include <iostream>
+
+class Deck {
+    private:
+	//may add later
+    public:
+	virtual void draw();
+	virtual void shuffle();
+	virtual void discardDeck();
+};
+#endif //__DECK_HPP__
