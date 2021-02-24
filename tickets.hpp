00b36d00cffdeafe51a8c5f5d5c736989e9d9032
ashley-kim22 <ashkim316@gmail.com>
2021-02-23 21:02:42 -0800

    Basics of ticketCard/Deck classes

---
+++ tickets.hpp
@@ -0,0 +1,27 @@
+#ifndef __TICKETS_HPP__
+#define __TICKETS_HPP__
+
+#include <string>
+
+using namespace std;
+
+class Tickets {
+
+public:
+
+    string DestinationA;
+    string DestinationB;
+
+    string getDestinationA() {
+   
+
+    }
+
+    string getDestinationB() {
+
+
+    }
+
+};
+
+#endif //__TICKETS_HPP__
